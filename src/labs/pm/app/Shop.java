21c6e3806529d694713fe4e3ac93a73d99057810
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-05-03 10:39:00 +0200

    Added Product Default constructor. Testing

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -4,6 +4,9 @@
  */
 package labs.pm.app;
 
+import java.math.BigDecimal;
+import labs.pm.data.Product;
+
 /**
  *
  * @author User
@@ -15,6 +18,14 @@ public class Shop {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Product p1 = new Product();
+        
+        p1.setId(101);
+        p1.setName("Tea");
+        p1.setPrice(BigDecimal.valueOf(1.99));
+        
+        System.out.println(p1.getId()+" "+ p1.getName()+" "+p1.getPrice());
+        
     }
     
 }
