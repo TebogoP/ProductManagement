964eefd92044083833093b68d5d6ce9e84314b8e
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-05-09 06:46:57 +0200

    Added documentation for getDiscount method in Drink and overrid the parent Product implementation.

--- src/labs/pm/data/Drink.java
+++ src/labs/pm/data/Drink.java
@@ -17,23 +17,43 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
+import java.time.LocalTime;
 
 /**
- * Represents a drink product in the inventory system.
- * Extends the base Product class.
+ * Represents a drink product in the inventory system. Extends the base Product
+ * class.
  *
  * @author User
  */
 public class Drink extends Product {
+
     /**
      * Constructor for creating a Drink with specified attributes.
      *
-     * @param id     The unique identifier for the drink
-     * @param name   The name of the drink
-     * @param price  The price of the drink
+     * @param id The unique identifier for the drink
+     * @param name The name of the drink
+     * @param price The price of the drink
      * @param rating The rating of the drink
      */
     public Drink(int id, String name, BigDecimal price, Rating rating) {
         super(id, name, price, rating);
     }
+
+    /**
+     * Calculates the discount for this product based on the current time. A
+     * discount is offered only during the "happy hour" between 17:30 and 18:30.
+     * During this time, the discount from the parent class is applied. At all
+     * other times, no discount (zero) is applied.
+     *
+     * @return The discount amount as a BigDecimal, or BigDecimal.ZERO if
+     * outside happy hour
+     */
+    @Override
+    public BigDecimal getDiscount() {
+        LocalTime now = LocalTime.now();
+        return (now.isAfter(LocalTime.of(17, 30))
+                && now.isBefore(LocalTime.of(18, 30)))
+                ? super.getDiscount() : BigDecimal.ZERO;
+    }
+
 }
